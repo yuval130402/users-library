5bda7266c699456439516581004501c71f1dcc10
Yuval Cohen <yuval130402@gmail.com>
2023-06-15 21:26:43 +0300

    update constants

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,2 @@
 export const API_BASE_URL = "https://randomuser.me/api/";
 export const USER_AMOUNT = 10;
-export const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS';
-export const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE';
