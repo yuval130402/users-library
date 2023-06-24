c46e0cbc58c51831f25912ef331a6009dfa7efae
yuval <yuval130402@gmail.com>
2023-06-25 02:36:59 +0300

    add some  constants - urls, and api request params

--- src/Constants.js
+++ src/Constants.js
@@ -1,2 +1,12 @@
 export const API_BASE_URL = "https://randomuser.me/api/";
-export const USER_AMOUNT = 10;
+export const USER_PER_PAGE = 12;
+export const MAX_USERS = 200;
+export const MAX_PAGE = Math.ceil(MAX_USERS / USER_PER_PAGE);
+export const SEED = "abc"; // same seed always generate the same set of users
+
+export const Urls = {
+    //AddUser: "/add-user",
+    Browse: "/browse",
+    Favorites: "/favorites",
+    Home: "/",
+}
