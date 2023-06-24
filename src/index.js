2bdd825940ee6e29aa3e465e4c9055ca4a7bfaa1
yuval <yuval130402@gmail.com>
2023-06-25 02:37:27 +0300

    add browserRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
-import store from "store/configureStore";
+import store from "context/store";
 import "./index.css";
 import App from "./App";
 
@@ -11,7 +12,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
