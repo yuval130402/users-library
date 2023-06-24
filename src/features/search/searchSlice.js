b2322d5ee338ed304c3aaaa8ee67517f836d2279
yuval <yuval130402@gmail.com>
2023-06-25 02:49:58 +0300

    add searchSlice with all the search reducers

---
+++ src/features/search/searchSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// Define the initial state using that type
+const initialState = {
+    searchQuery: '',
+};
+
+
+const searchSlice = createSlice({
+    name: 'search',
+    // `createSlice` will infer the state type from the `initialState` argument
+    initialState,
+    reducers: {
+        setSearchQuery: (state, action) => {
+            state.searchQuery = action.payload;
+        },
+
+        clearSearchQuery: (state) => {
+            state.searchQuery = "";
+        },
+    },
+});
+
+export const { setSearchQuery } = searchSlice.actions;
+
+export default searchSlice.reducer;
