e51d3554733fe196a2aba4cf955007b4eb62fec6
yuval <yuval130402@gmail.com>
2023-06-25 02:48:45 +0300

    add userSlice with all the reducers of users

---
+++ src/features/users/usersSlice.js
@@ -0,0 +1,62 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    users: [],
+    isLoading: false,
+    error: null,
+};
+
+const usersSlice = createSlice({
+    name: 'users',
+    initialState,
+    reducers: {
+        fetchUsersStart(state) {
+            state.isLoading = true;
+            state.error = null;
+        },
+
+        fetchUsersSuccess(state, action) {
+            state.users = action.payload;
+            state.isLoading = false;
+            state.error = null;
+            
+        },
+        
+        fetchUsersFailure(state, action) {
+            state.users = [];
+            state.isLoading = false;
+            state.error = action.payload;
+        },
+
+        updateUser(state, action) {
+            const { id, name, email, location } = action.payload;
+            const user = state.users.find((user) => user.id === id);
+            if (user) {
+              user.name = name;
+              user.email = email;
+              user.location = location;
+            }
+        },
+          
+        addUser(state, action) {
+            state.users.push(action.payload);
+        },
+          
+        deleteUser(state, action) {
+            const id = action.payload;
+            state.users = state.users.filter((user) => user.id !== id);
+        },
+    },
+});
+
+export const {
+    fetchUsersStart,
+    fetchUsersSuccess,
+    fetchUsersFailure,
+    updateUser,
+    addUser,
+    deleteUser,
+} = usersSlice.actions;
+
+
+export default usersSlice.reducer;
