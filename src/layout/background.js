10797f5e59c68bd44ca26baf0ded9798cec89f0b
yuval <yuval130402@gmail.com>
2023-06-25 02:44:51 +0300

    add background to the site

---
+++ src/layout/background.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'layout/background.css';
+
+function Background({children}) {
+  return (
+    <div className='background'>
+        {children}
+    </div>
+  )
+}
+
+export default Background;
