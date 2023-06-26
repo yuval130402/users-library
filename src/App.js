9fccb1290b5182a9fa947999e9bbdaefd87b1fc6
Yuval Cohen <yuval130402@gmail.com>
2023-06-26 13:27:15 +0300

    remove unused modules

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Urls } from "./utils/Constants";
 import UserLibraryPage from "pages/userLibraryPage";
 import FavoritesPage from "pages/favoritesPage";
